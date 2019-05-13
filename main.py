49d42103bedf9a4fc7c6e6bb6a2d938eea315f0f
大城慶知 <y.oshiro@churadata.okinawa>
2019-05-13 15:35:41 +0900

    uvloopでaysncioを高速化。autoreloaderで開発環境を整える

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from aiohttp import web
-from handlers import BaseHandler
+import aiohttp_autoreload
 import aiohttp_jinja2
 import jinja2
+import asyncio
+import uvloop
+
+from handlers import BaseHandler
+
+debug = True  # Or false
 
 def main():
-    app = web.Application()
+    # uvloop を用いてのasyncio高速化
+    loop = uvloop.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    app = web.Application(loop=loop)
+
+    # templates直下をjinjaでファイル読み込み
     aiohttp_jinja2.setup(app,
         loader=jinja2.FileSystemLoader('templates/'))
+
+    if debug:
+        aiohttp_autoreload.start()
     handler = BaseHandler()
     app.add_routes([web.get('/', handler.index),
                     web.get('/answer', handler.post)])
